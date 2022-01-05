21e72736512b46eca008d7cc2ba33f0210d716cb
Travis <tleedepriest@gmail.com>
2022-01-04 22:31:04 -0500

    adding scaffold for the circularlinkedlist

---
+++ circularllinkedlist.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        pass
+
+    def prepend(self, data):
+        pass
+
+    def print_list(self):
+        pass
